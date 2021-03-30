31e13a753c400d8f653defcce6910553b8048fe2
damianc <damianczapiewski@gmail.com>
2021-03-30 14:07:52 +0200

    add caching and dictionary as observer

--- README.md
+++ README.md
@@ -12,17 +12,70 @@ subject.subscribe(lambda res: print('A: ' + str(res)))
 subject.next(2)
 # A: 2
 
+subject.subscribe(lambda res: print('B: ' + str(res)))
+subject.next(3)
+# A: 3
+# B: 3
+```
+
+## Mappers
+
+```
 subject.pipe(
   lambda n: n + 1,
   lambda n: n * 10
 )
+```
+
+* in chain:
+
+```
+subject.pipe(
+  lambda n: n + 1
+).pipe(
+  lambda n: n * 10
+)
+```
+
+## Caching
+
+* to define how many values are to be cached, use the `cached` named parameter
+* to define initial cached values, use the `init` named parameter
+
+```
+subject = Subject(cache=2, init=[1, 2])
+subject.subscribe(lambda res: print('A: ' + str(res)))
+# A: 1
+# A: 2
 
 subject.next(3)
-# A: 40
+# A: 3
 
-subject.pipe(lambda n: n + 2).pipe(lambda n: n + 3)
 subject.subscribe(lambda res: print('B: ' + str(res)))
+# B: 2
+# B: 3
+
 subject.next(4)
-# A: 55
-# B: 55
+# A: 4
+# B: 4
 ```
+
+## Observers
+
+* function
+
+```
+subject.subscribe(lambda res: print(res))
+```
+
+* dictionary
+
+```
+subject.subscribe({
+  'next': lambda res: print(res),
+  'error': lambda err: print('ERROR:', err),
+  'complete': lambda: print('DONE')
+})
+```
+
+> All functions in the dictionary are optional.
--- subject.py
+++ subject.py
@@ -1,17 +1,99 @@
 class Subject:
   operators = []
   subs = []
+  cachedItems = []
+  cachedNum = 0
+  completed = False
 
+  '''
+  __init__
+  '''
+  def __init__(self, *, cached = 0, init = []):
+    if (cached > 0):
+      self.cachedNum = cached
+      if (len(init)):
+        self.cachedItems = [*init[-cached:]]
+
+  '''
+  pipe
+  '''
   def pipe(self, *ops):
     self.operators.extend(ops)
     return self
 
+  '''
+  next
+  '''
   def next(self, value):
+    if self.completed:
+      return
+
+    self.__putInCache(value)
+    value = self.__applyOperators(value)
+
+    for sub in self.subs:
+      if isinstance(sub, type({})):
+        if 'next' in sub:
+          sub['next'](value)
+      else:
+        sub(value)
+
+  '''
+  complete
+  '''
+  def complete(self):
+    if self.completed:
+      return
+
+    self.completed = True
+    for sub in self.subs:
+      if isinstance(sub, type({})):
+        if 'complete' in sub:
+          sub['complete']()
+
+  '''
+  error
+  '''
+  def error(self, message):
+    if self.completed:
+      return
+
+    errorHandlers = 0
+
+    for sub in self.subs:
+      if isinstance(sub, type({})):
+        if 'error' in sub:
+          errorHandlers += 1
+          sub['error'](message)
+
+    if (errorHandlers == 0):
+      raise Exception(message)
+
+  '''
+  subscribe
+  '''
+  def subscribe(self, fn):
+    self.subs.append(fn)
+
+    if (self.cachedNum > 0 and len(self.cachedItems)):
+      for cachedVal in self.cachedItems:
+        self.next(cachedVal)
+
+  '''
+  __applyOperators
+  '''
+  def __applyOperators(self, value):
     if (len(self.operators)):
       for op in self.operators:
         value = op(value)
-    for sub in self.subs:
-      sub(value)
 
-  def subscribe(self, fn):
-    self.subs.append(fn)
+    return value
+
+  '''
+  __putInCache
+  '''
+  def __putInCache(self, value):
+    if (len(self.cachedItems) < self.cachedNum):
+      self.cachedItems.append(value)
+    else:
+      self.cachedItems = self.cachedItems[1:] + [value]
